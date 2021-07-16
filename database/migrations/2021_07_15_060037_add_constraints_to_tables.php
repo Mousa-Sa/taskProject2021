1d9a775f59ec47bb2855719a46becef2816b44a4
Mousa-Sa <xdggasdg@gmail.com>
2021-07-16 04:29:41 +0300

    fix

--- database/migrations/2021_07_15_060037_add_constraints_to_tables.php
+++ database/migrations/2021_07_15_060037_add_constraints_to_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddConstraintToOrdersTables extends Migration
+class AddConstraintsToTables extends Migration
 {
     /**
      * Run the migrations.
